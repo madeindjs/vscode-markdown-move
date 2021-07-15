023fe28e2b472355c75a378765f1cbc5b0b13dcb
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-07-15 11:53:07 +0200

    move down works

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,26 @@
-import {commands, ExtensionContext} from "vscode";
+import {commands, ExtensionContext, Range, window} from "vscode";
 import {moveDown} from "./lib";
 
 export function activate(context: ExtensionContext) {
-  console.log('Congratulations, your extension "markdown-move" is now active!');
-
   let disposable = commands.registerCommand("markdown-move.move-down", () => {
-    console.log("hello");
-    moveDown();
+    const textEditor = window.activeTextEditor;
+
+    if (textEditor === undefined) {
+      window.showErrorMessage("You do not have any text editor opened");
+      return;
+    }
+
+    const content = textEditor.document.getText();
+    const position = textEditor.selection.active;
+
+    const begin = textEditor.document.positionAt(0);
+    const end = textEditor.document.positionAt(content.length);
+
+    const newContent = moveDown(content, position.character);
+
+    textEditor.edit((editBuilder) =>
+      editBuilder.replace(new Range(begin, end), newContent)
+    );
   });
 
   context.subscriptions.push(disposable);
