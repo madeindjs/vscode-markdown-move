77a617a3a1bea841c8c9955267c039f9f3c981d2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-01-02 21:47:56 +0100

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,11 @@
 import { commands, ExtensionContext, TextEditorEdit, window } from "vscode";
-import { demoteEditor, moveDownEditor, moveUpEditor, promoteEditor } from "./extension-moves";
-
-function showWarning(message: string): void {
-  window.showWarningMessage(`Markdown Move: ${message}`);
-}
+import { demoteEditor, moveDownEditor, moveUpEditor, promoteEditor } from "./extension-lib";
 
 function showError(message: string): void {
   window.showErrorMessage(`Markdown Move: ${message}`);
 }
 
-function moveAction(func: (line: string[], positionLine: number, editor: TextEditorEdit) => void): void {
+function baseAction(func: (line: string[], positionLine: number, editor: TextEditorEdit) => void): void {
   const textEditor = window.activeTextEditor;
 
   if (textEditor === undefined) {
@@ -19,41 +15,19 @@ function moveAction(func: (line: string[], positionLine: number, editor: TextEdi
   const content = textEditor.document.getText();
   const lines = content.split("\n");
 
-  textEditor.edit((editBuilder) => func(lines, textEditor.selection.active.line, editBuilder));
-}
-
-function promoteAction(): void {
-  const textEditor = window.activeTextEditor;
-
-  if (textEditor === undefined) {
-    return showError("You do not have any text editor opened");
+  try {
+    textEditor.edit((editBuilder) => func(lines, textEditor.selection.active.line, editBuilder));
+  } catch (e) {
+    console.error(e);
   }
-
-  const content = textEditor.document.getText();
-  const lines = content.split("\n");
-
-  textEditor.edit((editBuilder) => promoteEditor(lines, textEditor.selection.active.line, editBuilder));
-}
-
-function demoteAction(): void {
-  const textEditor = window.activeTextEditor;
-
-  if (textEditor === undefined) {
-    return showError("You do not have any text editor opened");
-  }
-
-  const content = textEditor.document.getText();
-  const lines = content.split("\n");
-
-  textEditor.edit((editBuilder) => demoteEditor(lines, textEditor.selection.active.line, editBuilder));
 }
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    commands.registerCommand("markdown-move.moveDown", () => moveAction(moveDownEditor)),
-    commands.registerCommand("markdown-move.moveUp", () => moveAction(moveUpEditor)),
-    commands.registerCommand("markdown-move.promote", promoteAction),
-    commands.registerCommand("markdown-move.demote", demoteAction)
+    commands.registerCommand("markdown-move.moveDown", () => baseAction(moveDownEditor)),
+    commands.registerCommand("markdown-move.moveUp", () => baseAction(moveUpEditor)),
+    commands.registerCommand("markdown-move.promote", () => baseAction(promoteEditor)),
+    commands.registerCommand("markdown-move.demote", () => baseAction(demoteEditor))
   );
 }
 
